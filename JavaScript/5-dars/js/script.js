06217f21c57de15db9eb63bfa2203e538351b586
murodjonabdujabbor33 <murodjonabdujabbor33>
2022-12-31 21:38:31 +0500

    add new

---
+++ JavaScript/5-dars/js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let number = 10; // o'zgartirish mumkin.
+const name = "Murodjon";
+
+number = 20;
+
+const person = {
+  name: "Yusuf",
+};
+
+person.name = "Murodjon";
+
+console.log(person)
