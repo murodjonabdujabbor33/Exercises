2a61a07a807ba25dffcc7a7f1769668c6115875c
murodjonabdujabbor33 <murodjonabdujabbor33>
2023-01-01 18:46:34 +0500

    murodjon

---
+++ 6-dars/js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let number = 5.6;
+
+// Number
+console.log(-8/0) // Infinity
+console.log('Murodjon' * 8) // NaN
+
+// String
+const name = "Murodjon1!";
+
+// Boolean
+const isMarried = false;
+// true = xa
+// false = yo'q
+
+// null
+console.log(lastname); // mavjud bo'lmagan o'zgaruvchini ishlatilsa.
+
+// undefined
+let und;
+console.log(und); // o'zgaruvchida hech qanday ma'lumot yo'q bo'lsa.
+
+
+
+
+// Object
+const thief = {
+  age: 30,
+  Jacked: "black",
+  LongHair: false,
+};
+
+console.log(thief.age);
+
+// Array
+const colors = ["red", "black", "green", 10];
+console.log(colors[1]);
