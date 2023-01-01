5ef88c5f37e07900b5b6859342860bb39457d308
murodjonabdujabbor33 <murodjonabdujabbor33>
2023-01-01 19:00:24 +0500

    add file

---
+++ JavaScript/10-dars/js/script.js
@@ -0,0 +1,62 @@
+"user strict";
+
+// qo'shish
+// console.log("Murodjon " + "Abdujabbor");
+// console.log(20 + "10");
+// console.log(20 + +"10");
+// console.log("Murodjon " + 8);
+// console.log(10 + 10);
+
+
+
+
+
+// incr, decr
+// let incr = 10;
+// let decr = 10;
+// incr++;
+// decr--;
+// console.log(incr);
+// console.log(decr);
+
+// console.log(++incr);
+// console.log(--decr);
+
+
+
+
+
+// qoldiqni topish (%)
+// console.log(5 % 2);
+
+
+
+
+
+// Boolean
+// console.log(5 * 5 == 25);
+// console.log(5 * 5 == "25"); // faqat qiymati solishtiriladi.
+// console.log(5 * 5 != 25);
+// console.log(5 * 5 === "25"); // qiymati va type solishtiriladi.
+
+
+
+
+
+// && = va   || = yoki
+// const isName = true;
+// const isLastname = true;
+// const isAge = false;
+// const isMarried = false;
+
+// console.log(isName && isLastname);
+// console.log(isName && isAge);
+// console.log(isName || isLastname);
+// console.log(isName || isAge);
+// console.log(isAge || isMarried);
+// console.log(isName && isLastname && !isAge); // ! false ni true ga aylantiradi.
+
+
+
+// console.log(2 + 2 * 2 !== "6"); // ham qiymatini ham ma'lumot turuni tekshiryapti.
+// console.log(2 + 2 * 2 != "6"); // bunda faqat qiymatlari tekshirilyapti.
