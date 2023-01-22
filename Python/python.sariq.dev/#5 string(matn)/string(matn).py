55a222269d82ee6014429ac04a2168989618d1a6
murodjonabdujabbor33 <murodjonabdujabbor33@gmail.com>
2023-01-22 18:36:00 +0500

    added tasks for the fifth and sixth lessons

---
+++ Python/python.sariq.dev/#5 string(matn)/string(matn).py
@@ -0,0 +1,38 @@
+# task1
+# kocha="Bog'bon"
+# mahalla="Sog'bon"
+# tuman="Bodomzor" 
+# viloyat="Samarqand"
+
+# Bog'bon ko'chasi, Sog'bon mahallasi, Bodomzor tumani, Samarqand viloyati
+
+# print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+
+# print(kocha+" ko'chasi "+mahalla+" mahallasi "+tuman+" tumani "+viloyat+" viloyati ")
+
+# task2
+# kocha=input("Ko'cha nomini kiriting: ")
+# mahalla=input("Mahall nomini kiriting: ")
+# tuman=input("Tuman nomini kiriting: ")
+# viloyat=input("Viloyat nomini kiriting: ")
+# print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+
+
+# task3
+# kocha="Bog'bon"
+# mahalla="Sog'bon"
+# tuman="Bodomzor" 
+# viloyat="Samarqand"
+# print(f"{kocha} ko'chasi\n {mahalla} mahallasi\n {tuman} tumani\n {viloyat} viloyati")
+
+
+# task4
+kocha="Bog'bon"
+mahalla="Sog'bon"
+tuman="Bodomzor" 
+viloyat="Samarqand"
+manzil = f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati"
+print(manzil.title())
+print(manzil.upper())
+print(manzil.lower())
+print(manzil.capitalize())
