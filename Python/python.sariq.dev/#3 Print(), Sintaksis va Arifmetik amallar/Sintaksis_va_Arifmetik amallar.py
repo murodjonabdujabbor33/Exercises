88558a895baa09b272c2095d70735cfef8ac2274
murodjonabdujabbor33 <murodjonabdujabbor33@gmail.com>
2023-01-22 18:29:50 +0500

    added tasks for the second, third and fourth lessons

---
+++ Python/python.sariq.dev/#3 Print(), Sintaksis va Arifmetik amallar/Sintaksis_va_Arifmetik amallar.py
@@ -0,0 +1,12 @@
+#1-vazifa
+
+print('"Nexia", "Tico", \'Damas ko\'rganlar qilar havas')
+
+
+#2-vazifa
+print("5 ning 4 darajasi: ", 5**4)
+print("22 ni 4 ga bo'lgandagi qoldiq: ", 22%4)
+print("Tomonlari 125 ga teng kvadratning yuzi: ", 125**2)
+print("Tomonlari 125 ga teng bo'lgan kvadrating peremetri: ", 4*125)
+print("Diametri 12 ga teng bo'lgan doiraning yuzi: ", 12/6*3.14**2)
+print("Katetlari 6 va 7 ga teng bo'lgan to'g'ri burchakli uchburchakning gipzatenuzasi: ", (6**2+7**2)**1/2)
---
+++ Python/python.sariq.dev/#4 O'zgaruvchilar/ozgaruvchilar.py
@@ -0,0 +1,11 @@
+hello = "Hello World"
+print(hello)
+xabar = "Bag'dodda hammayoq tinch"
+print(xabar)
+xabar = "Mashina"
+print(xabar)
+
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi", radius, "ga teng aylananing yuzi: ", aylana_yuzi)
