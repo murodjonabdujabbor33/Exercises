e981f7bfb25645a4d3c26d0341235b93c1103ae4
murodjonabdujabbor33 <murodjonabdujabbor33@gmail.com>
2023-02-17 10:02:27 +0500

    Added exercises and tasks for lesson 5.

--- JavaScript/Samar-Badriddinov/3-dars/js/script.js
+++ JavaScript/Samar-Badriddinov/3-dars/js/script.js
@@ -1,1 +1,1 @@
-alert("Hello Murodjon")
+alert("Hello Murodjon");
--- JavaScript/Samar-Badriddinov/5-dars/js/script.js
+++ JavaScript/Samar-Badriddinov/5-dars/js/script.js
@@ -1,14 +1,40 @@
 "use strict";
 
+// O'zgaruvchi yaratishning 1-usuli:
+
 let number = 10; // o'zgartirish mumkin.
-const name = "Murodjon";
-
+const myName = "Murodjon"; // Came Case usulidagi o'zgaruvchi
+console.log(number);
 number = 20;
+console.log(number);
 
+// const qiymatlarni object yaratish orqali o'zgartirish mumkin.
 const person = {
   name: "Yusuf",
 };
 
 person.name = "Murodjon";
+console.log(person);
 
-console.log(person)
+
+// O'zgaruvchi yaratishning 2-usuli: (eski versiya)
+
+var clientName = "Samar";
+console.log(clientName);
+clientName = "Murodjon";
+console.log(clientName);
+
+// var ishlatilmasligining sabablaridan bittasi agar biz o'zgaruvchini u yaratilishidan oldin e'lon qilib qo'ysak, xatolik o'rniga undefined chiqadi.
+
+console.log(lastName);
+var lastName = "Abdujabbor";
+
+
+// "use strict" siz:
+number = 12;
+console.log(number);
+
+// "use strict" bilan:
+"use strict";
+number = 23;
+console.log(number);
