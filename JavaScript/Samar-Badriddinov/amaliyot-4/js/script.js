677acce58a46e274d4b56602d07ba7da3a73e20d
murodjonabdujabbor33 <murodjonabdujabbor33@gmail.com>
2023-02-23 11:49:29 +0500

    The tasks of the practice were added.

--- JavaScript/Samar-Badriddinov/amaliyot-4/js/script.js
+++ JavaScript/Samar-Badriddinov/amaliyot-4/js/script.js
@@ -1,76 +1,102 @@
 "use strict";
 
-// FOR ORQALI
 
 
 // vazifa-1
-let NumberOfSeries;
-function startApp() {
-  while (NumberOfSeries == "" || NumberOfSeries == null || isNaN(NumberOfSeries)) {
-    NumberOfSeries = prompt("Nechta serial ko'rdingiz", "");
+const seriesDB = {
+  count: 0,
+  sereis: {}, // object
+  actors: {}, // object
+  genres: [], // massiv
+  private: false,
+  start: function startApp() {
+    seriesDB.count = +prompt("Nechta serial ko'rdingiz", "");
+    while (seriesDB.count == "" || seriesDB.count == null || isNaN(seriesDB.count)) {
+      seriesDB.count = +prompt("Nechta serial ko'rdingiz", "");
+    }
+  },
+visableDB: function changePrivate() {
+  if (seriesDB.private === true) {
+    seriesDB.private = false;
+  } else if (seriesDB.private === false) {
+    seriesDB.private = true;
+  } else {
+    console.log("Sizning private xususiyatingizga boolen qiymat kiritilmagan.");
   }
-}
-startApp();
-
-// vazifa-2
-const SeriesDB = {
-  count: NumberOfSeries,
-  Sereis: {}, // object
-  Actors: {}, // object
-  Genres: [], // massiv
-  Private: false,
-};
-
-// vazifa-3
-function rememberMySeries() {
-  for(let i = 0; i < 2; i++) {
-    const SeriesName = prompt("Oxirgi ko'rgan ko'rgan serialingiz?"),
-      SeriesRating = prompt("Ushbu serialga nechta baho qo'yasiz?");
-
-      if (SeriesName != null && SeriesRating != null && SeriesName != "" && SeriesRating != "") {
-        SeriesDB.Sereis[SeriesName] = SeriesRating;  // property: value;
-        console.log("done");
-      }
-      else {
-        console.log("error");
-        i--;
-      }
-  }
-}
-// rememberMySeries();
-
-
-function detectLevelSeries() {
-  if (SeriesDB.count < 5) {
-    console.log("Kam serial ko'ribsiz.");
-  }
-  else if (SeriesDB.count >= 5 && SeriesDB.count < 10) {
-    console.log("Siz klassik tomoshabin ekansiz.");
-  }
-  else if (SeriesDB.count >= 10) {
-    console.log("Siz Serilachi zvezda ekansiz.");
-  }
-  else {
-    console.log("Error");
-  }
-}
-detectLevelSeries();
-
-
-function ShowDB(hidden) {
-  if (!hidden) {
-    console.log(SeriesDB);
-  }
-}
-ShowDB(SeriesDB.Private);
-
-
-function writeGenres() {
+},
+writeGenres: function countGenres() {
   for (let i = 0; i < 3; i++) {
     const movieGenre = prompt(`Yaxshi ko'rgan janringiz ${i+1}`);
-    SeriesDB.Genres[i] = movieGenre;
+    seriesDB.genres[i] = movieGenre;
+    if (seriesDB.genres[i] === "" || seriesDB.genres[i] === null) {
+      i--;
+    }
   }
-}
-writeGenres();
+  seriesDB.genres.forEach(function(genre, index) {
+    console.log(`Yaxshi ko'rgan ${index} janringiz - ${genre}`);
+  });
+},
+};
 
-console.log(SeriesDB);
+seriesDB.start();
+seriesDB.visableDB();
+seriesDB.writeGenres();
+console.log(seriesDB);
+
+
+
+
+
+// // vazifa-3
+// function rememberMySeries() {
+//   for(let i = 0; i < 2; i++) {
+//     const SeriesName = prompt("Oxirgi ko'rgan ko'rgan serialingiz?"),
+//       SeriesRating = prompt("Ushbu serialga nechta baho qo'yasiz?");
+
+//       if (SeriesName != null && SeriesRating != null && SeriesName != "" && SeriesRating != "") {
+//         SeriesDB.Sereis[SeriesName] = SeriesRating;  // property: value;
+//         console.log("done");
+//       }
+//       else {
+//         console.log("error");
+//         i--;
+//       }
+//   }
+// }
+// // rememberMySeries();
+
+
+// function detectLevelSeries() {
+//   if (SeriesDB.count < 5) {
+//     console.log("Kam serial ko'ribsiz.");
+//   }
+//   else if (SeriesDB.count >= 5 && SeriesDB.count < 10) {
+//     console.log("Siz klassik tomoshabin ekansiz.");
+//   }
+//   else if (SeriesDB.count >= 10) {
+//     console.log("Siz Serilachi zvezda ekansiz.");
+//   }
+//   else {
+//     console.log("Error");
+//   }
+// }
+// detectLevelSeries();
+
+
+// function ShowDB(hidden) {
+//   if (!hidden) {
+//     console.log(SeriesDB);
+//   }
+// }
+// ShowDB(SeriesDB.Private);
+
+
+// function writeGenres() {
+//   for (let i = 0; i < 3; i++) {
+//     const movieGenre = prompt(`Yaxshi ko'rgan janringiz ${i+1}`);
+//     SeriesDB.Genres[i] = movieGenre;
+//   }
+// }
+// writeGenres();
+
+// console.log(SeriesDB);
