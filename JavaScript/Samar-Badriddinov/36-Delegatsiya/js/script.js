25c7e86c6227484c65d9abcd00de62295a78d1a5
murodjonabdujabbor33 <murodjonabdujabbor33@gmail.com>
2023-03-08 15:27:21 +0500

    exercises for JS delegatsiya

--- JavaScript/Samar-Badriddinov/36-Delegatsiya/js/script.js
+++ JavaScript/Samar-Badriddinov/36-Delegatsiya/js/script.js
@@ -1,64 +1,84 @@
 "use strict";
 
 // qaysi element bosilsa, u haqidagi block ma'lumotlar chiqadi.
-const wrapper = document.querySelector('.btn-block');
-wrapper.addEventListener('click', (event) => {
-  console.dir(event.target);
-});
+// const wrapper = document.querySelector('.btn-block');
+// wrapper.addEventListener('click', (event) => {
+//   console.dir(event.target);
+// });
+
 
 
 
 // faqatgina knopkani bosgandagina ma'lumot chiqarish:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ona elementdan turib bola elementga qandadir xodisa qo'shishimiz mumkin.
-
-// const wrapper = document.querySelector('.btn-block'),
-//   btns = document.querySelectorAll('button');
-
-
-// btns[0].addEventListener('click', () => {
-//   btns[1].classList.toggle('red');
-// });
-
-// wrapper.addEventListener("click", (event) => {
-//   if (event.target && event.target.classList.contains('red')) {
+// const wrapper = document.querySelector('.btn-block');
+// wrapper.addEventListener('click', (event) => {
+//   if (event.target && event.target.TagName === 'BUTTON') {
 //     console.log('btn click');
 //   }
 // });
 
-// const btn = document.createElement('button');
-// btn.classList.add('red');
-// wrapper.append(btn);
+
+
+
+
+// biror classni bosgandagina ishlashi:
+// const wrapper = document.querySelector('.btn-block');
+// wrapper.addEventListener('click', (event) => {
+//   if (event.target && event.target.classList.contains('blue')) {
+//     console.log('btn click');
+//   }
+// });
+
 
 
 
 
 // delegatsiyaning muhimligiga sabab:
-
 // btns.forEach((item) => {
 //   item.addEventListener("click", () => {
 //     console.log('clicked');
 //   });
 // });
-
 // const btn = document.createElement('button');
 // btn.classList.add('red');
 // wrapper.append(btn);
+
+
+
+
+
+
+
+// delegatsiya -> ona elementdan turib bola elementga qandadir xodisa qo'shishimiz mumkin.
+
+// const wrapper = document.querySelector('.btn-block'),
+//   btns = document.querySelectorAll('button');
+
+// btns[0].addEventListener('click', () => {
+//   btns[1].classList.toggle('red');
+// });
+// wrapper.addEventListener("click", (event) => {
+//   if (event.target && event.target.classList.contains('red')) {
+//     console.log('btn click');
+//   }
+// });
+// const btn = document.createElement('button');
+// btn.classList.add('red');
+// wrapper.append(btn);
+
+
+
+const wrapper = document.querySelector('.btn-block'),
+  btns = document.querySelectorAll('button');
+
+btns[0].addEventListener('click', () => {
+  btns[1].classList.toggle('red');
+});
+wrapper.addEventListener("click", (event) => {
+  if (event.target && event.target.matches('button.red')) {
+    console.log('btn click');
+  }
+});
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
